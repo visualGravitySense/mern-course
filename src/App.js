82aa5e71851ba07b6749a33fb3780412a5c38fd3
visualgravitysense <helikeel@gmail.com>
2022-01-07 01:08:32 +0200

    react mini service todo list digo.ee 1st frontend test part

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import TodoList from './Todo/TodoList'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const [todos, setTodos] = React.useState([
+        {id: 1, completed: false, title: "Купить Хлеб" },
+        {id: 2, completed: false, title: "Купить Масло" },
+        {id: 3, completed: false, title: "Купить Молоко" },
+    ])
+
+    function toggleTodo(id) {
+        setTodos(
+            todos.map(todo => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed
+            }
+            return todo
+            })
+        )
+    }
+
+  return (<div className='wrapper'>
+        <h1>React Tutorial</h1>
+          <TodoList todos={todos}  onToggle={toggleTodo}/>
+      </div>
   );
 }
 
---
+++ src/Todo/TodoItem.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import TodoList from "./TodoList"
+
+const styles = { li:{display:'flex',justifyContent:'space-between',
+    alignItems: 'center', padding: '.5rem 1rem', border: '1px solid #ccc',
+    borderRadius: '4px', marginBottom: '.5rem'},
+    input: {marginRight: '1rem'}
+
+}
+
+function TodoItem({ todo, index, onChange }) {
+
+    return (
+        <li style={styles.li}>
+        <span>
+            <input type="checkbox" style={styles.input}
+            onChange={() => onChange(todo.id)}/>
+
+        <strong>{index + 1}</strong>
+        &nbsp;
+        {todo.title}
+        </span>
+        <button className='rm'>&times;</button>
+        </li>
+)
+}
+
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired,
+    index: PropTypes.number,
+    onChange: PropTypes.func.isRequired
+}
+
+export default TodoItem
