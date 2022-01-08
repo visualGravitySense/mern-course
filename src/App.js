edf3f8ba3406277cb8cc09a249ec0a07d36ede9e
visualgravitysense <helikeel@gmail.com>
2022-01-08 22:27:17 +0200

    Digo.ee mini react service: 1. Todo List 2. Counters

--- src/App.js
+++ src/App.js
@@ -1,55 +1,91 @@
-import React from 'react'
-import TodoList from './Todo/TodoList'
+// import React, { useEffect } from 'react';
+import React, { useState } from 'react';
+import TodoList from './Todo/TodoList';
 import Context from "./context";
-import AddTodo from "./Todo/AddTodo";
+import AddTodo from "./Todo/AddTodo"
+import Loader from "./Loader"
 import {setSelectionRange} from "@testing-library/user-event/dist/utils";
+import Counter from "./components/Counter";
 
 function App() {
-    const [todos, setTodos] = React.useState([
-        {id: 1, completed: false, title: "Купить Хлеб" },
-        {id: 2, completed: false, title: "Купить Масло" },
-        {id: 3, completed: false, title: "Купить Молоко" },
-    ])
-
-    function toggleTodo(id) {
-        // console.log('todo id', id)
-        setTodos(
-            todos.map(todo => {
-            if (todo.id === id) {
-                todo.completed = !todo.completed
-            }
-            return todo
-            })
-        )
-    }
-
-    function removeTodo(id) {
-        setTodos(todos.filter(todo => todo.id !== id))
-    }
-
-
-    function addTodo(title) {
-        setTodos(
-            todos.concat([
-                {
-                    title,
-                    id: Date.now(),
-                    completed: false
-                }
-        ]))
-    }
+    const [likes, setLikes] = useState(5)
+    const [value, setValue] = useState('Текст в Инпуте')
     
-  return (
-      <Context.Provider value={{ removeTodo }}>
-          <div className='wrapper'>
-            <h1>React Tutorial</h1>
-              <AddTodo onCreate={addTodo}/>
 
-              {todos.length ? <TodoList todos={todos}  onToggle={toggleTodo} /> : <p>No todos</p> }
 
-          </div>
-      </Context.Provider>
-  );
-}
+    return (
+        <div className="App">
+            <Counter />
+            <Counter />
+            <Counter />
+            <Counter />
+
+        </div>
+    );
+
+    }
+
+
 
 export default App;
+
+// TODO_APP
+// function App() {
+//     const [todos, setTodos] = React.useState([])
+//     const [loading, setLoading] = React.useState(true)
+//
+//     useEffect(() =>{
+//         fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+//             .then(response => response.json())
+//             .then(todos => {
+//                 setTimeout(() => {
+//                     setTodos(todos)
+//                     setLoading(false)
+//                 }, 2000)
+//             })
+//     }, [])
+//
+//     function toggleTodo(id) {
+//         // console.log('todo id', id)
+//         setTodos(
+//             todos.map(todo => {
+//             if (todo.id === id) {
+//                 todo.completed = !todo.completed
+//             }
+//             return todo
+//             })
+//         )
+//     }
+//
+//     function removeTodo(id) {
+//         setTodos(todos.filter(todo => todo.id !== id))
+//     }
+//
+//
+//     function addTodo(title) {
+//         setTodos(
+//             todos.concat([
+//                 {
+//                     title,
+//                     id: Date.now(),
+//                     completed: false
+//                 }
+//         ]))
+//     }
+//
+//   return (
+//       <Context.Provider value={{ removeTodo }}>
+//           <div className='wrapper'>
+//             <h1>React Tutorial</h1>
+//               <AddTodo onCreate={addTodo} />
+//
+//               {loading && <Loader />}
+//
+//               {todos.length ? ( <TodoList todos={todos}  onToggle={toggleTodo} /> ) : ( loading ? null : <p>No todos</p> ) }
+//
+//           </div>
+//       </Context.Provider>
+//   );
+// }
+//
+// export default App;
--- src/Todo/AddTodo.js
+++ src/Todo/AddTodo.js
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
-function AddTodo(onCreate) {
+function useInputValue(defaultValue = '') {
+    const [value, setValue] = useState(defaultValue)
+
+    return {
+        bind: {
+            value,
+            onChange: event => setValue(event.target.value)
+        },
+        clear: () => setValue(''),
+        value: () => value
+    }
+}
+
+function AddTodo({ onCreate }) {
+    const input = useInputValue('')
 
     function submitHandler(event) {
         event.preventDefault()
 
-        if (value.trim()) {
-            onCreate(value)
+        if (input.value.trim()) {
+            onCreate(input.value)
+            input.clear()
         }
     }
 
     return (
         <form style={{ marginBottom: '1rem' }} onSubmit={submitHandler} >
-            <input value={value} onChange={event => setValue(event.target.value)}/>
+            <input {...input.bind} />
             <button type='submit'>Add Todo</button>
         </form>
     )
